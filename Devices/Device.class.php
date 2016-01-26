a77e3f9f62a722ea35851edf35e3b955a3f0a151
florianprobst <florian.probst@gmx.de>
2016-01-26 10:24:35 +0100

    changed washing machine class to general device class

---
+++ Devices/Device.class.php
@@ -0,0 +1,34 @@
+<?
+/**
+ * Implementation
+ * 
+ *
+ * @link https://github.com/florianprobst/ips-energymanager project website
+ * 
+ * @author Florian Probst <florian.probst@gmx.de>
+ * 
+ * @license GNU
+ * GNU General Public License, version 3
+ */
+
+require_once 'AbstractDevice.class.php';
+//require_once '/ips-library/IPSVariable.class.php';
+
+/**
+* class HomeMaticPowerMeterHM_ES_PMSw1_Pl
+* 
+* @uses AbstractPowerMeter as parent class
+*/
+class Device extends AbstractDevice{
+	
+	public function __construct($name, $powermeter, $standbylevel, $poweronlevel, $manufacturer = self::UNKNOWN_MANUFACTURER, $model = self::UNKNOWN_MODEL){
+		parent::__construct($name, $powermeter, $standbylevel, $poweronlevel, $manufacturer, $model);
+		
+		
+		echo "im in the washing machine dafuq";
+		//we need the following shit:
+		//events checking the state of the washing machine.
+		//a variable holding the running state of the washing machine
+		//notification to cell phone apps
+	}
+}
